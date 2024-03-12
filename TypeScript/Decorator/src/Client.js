7a98df86d6d981aed449060a8505a98da7ed8e77
BOUY CHAN DARA <71806973+Dara-Coder@users.noreply.github.com>
2024-03-13 02:04:12 +0700

    Delete TypeScript/Decorator/src directory

--- TypeScript/Decorator/src/Client.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.client = void 0;
-function client(component) {
-    return component.operation();
-}
-exports.client = client;
